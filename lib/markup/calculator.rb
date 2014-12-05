de31d1d007ed0a33606784389c15f20c4a2eb395
Ernesto Soberanes <netosober@gmail.com>
2014-12-05 11:45:40 -0500

    Add tests for Calculator#initialize

--- lib/markup/calculator.rb
+++ lib/markup/calculator.rb
@@ -1,5 +1,15 @@
 module Markup
   class Calculator
+    attr_reader :flat_rate
+
+    def initialize(flat_rate)
+       if (flat_rate < 0)
+        raise "flat_rate cannot be negative"
+      end
+
+      @flat_rate = flat_rate
+    end
+
     def process(price)
       if (price<0)
         raise "Price cannot be negative"
--- spec/markup/calculator_spec.rb
+++ spec/markup/calculator_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module Markup
   describe Calculator do
-    let(:calculator) {Calculator.new}
+    let(:calculator) {Calculator.new 0.5}
     describe "#process" do
       it "applies a flat rate" do
         result = calculator.process 100
@@ -10,6 +10,14 @@ module Markup
       end
       it "raises error with negative input" do
         expect {calculator.process(-1)}.to raise_error
+      end
+    end
+    describe "#initialize" do
+      it "set a flat rate" do
+        expect(calculator.flat_rate).to eq 0.5
+      end
+      it "raises error with negative flat rate" do
+        expect {Calculator.new -1}.to raise_error
       end
     end
   end
