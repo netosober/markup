aac94ab8070800678d4433d418232d77c6e14b0a
Ernesto Soberanes <netosober@gmail.com>
2014-12-05 14:58:55 -0500

    Add exception tests for worker_markup

--- lib/markup/worker_markup.rb
+++ lib/markup/worker_markup.rb
@@ -11,6 +11,13 @@ module Markup
     end
 
     def calculate(price, workers_quantity)
+      if (price < 0)
+        raise "price cannot be negative"
+      end
+      if (workers_quantity < 0)
+        raise "workers_quantity cannot be negative"
+      end
+
       price*worker_rate*workers_quantity
     end
   end
--- spec/markup/worker_markup_spec.rb
+++ spec/markup/worker_markup_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 module Markup
   describe WorkerMarkup do
     let(:worker_markup) {WorkerMarkup.new 0.012}
+    let(:price) {100}
 
     describe "#initialize" do
       it "set a worker rate" do
@@ -14,8 +15,18 @@ module Markup
     end
     describe "#calculate" do
       it "returns markup for one worker" do
-        result = worker_markup.calculate(100, 1)
-        expect(result).to eq(1.2)
+        result = worker_markup.calculate(price, 1)
+        expect(result).to be_within(0.01).of(1.2)
+      end
+      it "returns markup for one worker" do
+        result = worker_markup.calculate(price, 3)
+        expect(result).to be_within(0.01).of(3.6)
+      end
+      it "raises error with negative price" do
+        expect {worker_markup.calculate(-1,1)}.to raise_error
+      end
+      it "raises error with negative workers quantity" do
+        expect {worker_markup.calculate(1,-1)}.to raise_error
       end
     end
   end
