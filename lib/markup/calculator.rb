0bac22bb505c1e9e46231ac857892ccdb2238641
Ernesto Soberanes <netosober@gmail.com>
2014-12-05 15:45:30 -0500

    Add integration of worker_markup to calculator

--- lib/markup/calculator.rb
+++ lib/markup/calculator.rb
@@ -1,20 +1,25 @@
 module Markup
   class Calculator
-    attr_reader :flat_rate
+    attr_reader :flat_rate, :worker_markup
 
-    def initialize(flat_rate)
+    def initialize(flat_rate, worker_markup)
       if (flat_rate < 0)
         raise "flat_rate cannot be negative"
       end
 
+      @worker_markup = worker_markup
       @flat_rate = flat_rate
     end
 
-    def process(price)
+    def process(price, workers_quantity)
       if (price<0)
         raise "Price cannot be negative"
       end
-      price + price * @flat_rate
+      subtotal = price + price * @flat_rate
+
+      worker_markup_result = @worker_markup.calculate(subtotal, workers_quantity)
+
+      subtotal + worker_markup_result
     end
   end
 end
--- spec/markup/calculator_spec.rb
+++ spec/markup/calculator_spec.rb
@@ -2,23 +2,46 @@ require 'spec_helper'
 
 module Markup
   describe Calculator do
-    let(:calculator) {Calculator.new 0.05}
-    let(:zero_rate_calculator) {Calculator.new 0}
+    let(:worker_markup) {double("WorkerMarkup")}
+    let(:calculator) {Calculator.new 0.05, worker_markup}
+    let(:zero_rate_calculator) {Calculator.new 0, worker_markup}
 
     describe "#process" do
       let(:price) {100}
+      let(:workers_quantity) {3}
       context "with normal rate of 5%" do
+        before(:each) do
+          allow(worker_markup).to receive(:calculate).with(105, workers_quantity) { 0 }
+        end
+
         it "applies a flat rate" do
-          result = calculator.process price
+          result = calculator.process(price, workers_quantity)
           expect(result).to eq(105)
         end
         it "raises error with negative input" do
-          expect {calculator.process(-1)}.to raise_error
+          expect {calculator.process(-1, 3)}.to raise_error
+        end
+        it "calls worker_markup#calculate with 3 workers" do
+          expect(worker_markup).to receive(:calculate).with(105, 3)
+          calculator.process(price, workers_quantity)
+        end
+        it "calls worker_markup#calculate with price + flat markup" do
+          expect(worker_markup).to receive(:calculate).with(105, 3)
+          calculator.process(price, workers_quantity)
+        end
+        it "adds the result of worker_markup#calculate to price + flat markup" do
+          expect(worker_markup).to receive(:calculate).with(105, 3) {5}
+          result = calculator.process(price, workers_quantity)
+          expect(result).to eq(110)
         end
       end
       context "with flat rate of 0" do
+        before(:each) do
+          allow(worker_markup).to receive(:calculate).with(100, workers_quantity) { 0 }
+        end
+
         it "doesn't increase the price" do
-          result = zero_rate_calculator.process price
+          result = zero_rate_calculator.process(price, workers_quantity)
           expect(result).to eq(price)
         end
       end
@@ -28,7 +51,10 @@ module Markup
         expect(calculator.flat_rate).to eq 0.05
       end
       it "raises error with negative flat rate" do
-        expect {Calculator.new -1}.to raise_error
+        expect {Calculator.new(-1, worker_markup)}.to raise_error
+      end
+      it "set a worker_markup" do
+        expect(calculator.worker_markup).to eql(worker_markup)
       end
     end
   end
