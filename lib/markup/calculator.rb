bb8ae4df502605350306c9a5e68161d31104fa85
Ernesto Soberanes <netosober@gmail.com>
2014-12-05 21:04:06 -0500

    Add material_markup tests to calculator

--- lib/markup/calculator.rb
+++ lib/markup/calculator.rb
@@ -1,25 +1,26 @@
 module Markup
   class Calculator
-    attr_reader :flat_rate, :worker_markup
+    attr_reader :flat_rate, :worker_markup, :material_markup
 
-    def initialize(flat_rate, worker_markup)
+    def initialize(flat_rate, worker_markup, material_markup)
       if (flat_rate < 0)
         raise "flat_rate cannot be negative"
       end
 
       @worker_markup = worker_markup
+      @material_markup = material_markup
       @flat_rate = flat_rate
     end
 
-    def process(price, workers_quantity)
+    def process(price, workers_quantity, material_type)
       if (price<0)
         raise "Price cannot be negative"
       end
       subtotal = price + price * @flat_rate
 
       worker_markup_result = @worker_markup.calculate(subtotal, workers_quantity)
-
-      subtotal + worker_markup_result
+      material_markup_result = @material_markup.calculate(subtotal, material_type)
+      subtotal + worker_markup_result + material_markup_result
     end
   end
 end
--- spec/markup/calculator_spec.rb
+++ spec/markup/calculator_spec.rb
@@ -3,45 +3,55 @@ require 'spec_helper'
 module Markup
   describe Calculator do
     let(:worker_markup) {double("WorkerMarkup")}
-    let(:calculator) {Calculator.new 0.05, worker_markup}
-    let(:zero_rate_calculator) {Calculator.new 0, worker_markup}
+    let(:material_markup) {double("MaterialMarkup")}
+
+    let(:calculator) {Calculator.new 0.05, worker_markup, material_markup }
+    let(:zero_rate_calculator) {Calculator.new 0, worker_markup, material_markup }
 
     describe "#process" do
       let(:price) {100}
       let(:workers_quantity) {3}
+      let(:material_type) {'food'}
       context "with normal rate of 5%" do
         before(:each) do
           allow(worker_markup).to receive(:calculate).with(105, workers_quantity) { 0 }
+          allow(material_markup).to receive(:calculate).with(105, material_type) { 0 }
         end
 
         it "applies a flat rate" do
-          result = calculator.process(price, workers_quantity)
+          result = calculator.process(price, workers_quantity, material_type)
           expect(result).to eq(105)
         end
         it "raises error with negative input" do
-          expect {calculator.process(-1, 3)}.to raise_error
+          expect {calculator.process(-1, 3, "material")}.to raise_error
         end
-        it "calls worker_markup#calculate with 3 workers" do
+        it "calls worker_markup#calculate with 105 price and 3 workers" do
           expect(worker_markup).to receive(:calculate).with(105, 3)
-          calculator.process(price, workers_quantity)
-        end
-        it "calls worker_markup#calculate with price + flat markup" do
-          expect(worker_markup).to receive(:calculate).with(105, 3)
-          calculator.process(price, workers_quantity)
+          calculator.process(price, workers_quantity, material_type)
         end
         it "adds the result of worker_markup#calculate to price + flat markup" do
-          expect(worker_markup).to receive(:calculate).with(105, 3) {5}
-          result = calculator.process(price, workers_quantity)
+          allow(worker_markup).to receive(:calculate).with(105, 3) {5}
+          result = calculator.process(price, workers_quantity, material_type)
+          expect(result).to eq(110)
+        end
+        it "calls material_markup#calculate with 105 price and 'food' material" do
+          expect(material_markup).to receive(:calculate).with(105, material_type)
+          calculator.process(price, workers_quantity, material_type)
+        end
+        it "adds the result of worker_markup#calculate to price + flat markup" do
+          allow(material_markup).to receive(:calculate).with(105, material_type) {5}
+          result = calculator.process(price, workers_quantity, material_type)
           expect(result).to eq(110)
         end
       end
       context "with flat rate of 0" do
         before(:each) do
           allow(worker_markup).to receive(:calculate).with(100, workers_quantity) { 0 }
+          allow(material_markup).to receive(:calculate).with(100, material_type) { 0 }
         end
 
         it "doesn't increase the price" do
-          result = zero_rate_calculator.process(price, workers_quantity)
+          result = zero_rate_calculator.process(price, workers_quantity, material_type)
           expect(result).to eq(price)
         end
       end
