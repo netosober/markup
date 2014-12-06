3e5b771fa0a8af81dcc37305354b603653588985
Ernesto Soberanes <netosober@gmail.com>
2014-12-05 20:50:01 -0500

    Add material_markup#calculate tests

--- lib/markup/material_markup.rb
+++ lib/markup/material_markup.rb
@@ -1,8 +1,22 @@
 module Markup
   class MaterialMarkup
     attr_reader :material_rates
+
     def initialize(material_rates={})
       @material_rates = material_rates
+    end
+
+    def calculate(price, material)
+      if (price < 0)
+        raise "price cannot be negative"
+      end
+
+      rate = material_rates[material]
+      if rate
+        price*rate
+      else
+        0
+      end
     end
   end
 end
--- spec/markup/material_markup_spec.rb
+++ spec/markup/material_markup_spec.rb
@@ -2,15 +2,29 @@ require 'spec_helper'
 
 module Markup
   describe MaterialMarkup do
+    let(:material_rates) { { "drugs" => 0.075, "food" => 0.13, "electronics" => 0.02 } }
+    let(:material_markup) { MaterialMarkup.new material_rates }
+
     describe "#initialize" do
       it "sets material rates" do
-        material_rates =  { "drugs" => 0.075, "food" => 0.13, "electronics" => 0.02 }
-        material_markup = MaterialMarkup.new material_rates
         expect(material_markup.material_rates).to eql(material_rates)
       end
       it "sets empty rates for no parameter" do
         no_rates_material_markup = MaterialMarkup.new
         expect(no_rates_material_markup.material_rates).to eq({})
+      end
+    end
+    describe "#calculate" do
+      it "returns markup for known material" do
+        result = material_markup.calculate(100, "drugs")
+        expect(result).to within(0.01).of(7.5)
+      end
+      it "returns 0 markup for unknown material" do
+        result = material_markup.calculate(100, "air")
+        expect(result).to eq(0)
+      end
+      it "raises error if price is negative" do
+        expect {material_markup.calculate(-1, "drugs")}.to raise_error
       end
     end
   end
